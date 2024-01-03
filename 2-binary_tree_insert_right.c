2b84e095cd6e89f67811ecbbe80c860bdb59dfbb
prisca2920 <prisca2920@gmail.com>
2024-01-03 01:28:36 -0800

    right child

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -8,3 +8,23 @@
  */
 
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode;
+
+	if (parent == NULL)
+	{
+	return (NULL);
+	}
+
+	newnode = binary_tree_node(parent, value);
+
+	if (parent->right != NULL)
+	{
+	newnode->right = parent->right;
+	newnode->right->parent = newnode;
+	}
+
+	parent->right = newnode;
+
+	return (newnode);
+}
