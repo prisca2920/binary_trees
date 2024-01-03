3daa4ca1465cd32c902949eb5c92d159ae849bdb
prisca2920 <prisca2920@gmail.com>
2024-01-03 09:18:56 -0800

    height

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -7,3 +7,33 @@
  */
 
 size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t h_left = 0;
+	size_t h_right = 0;
+
+	if (tree == NULL)
+	{
+	return (0);
+	
+	}
+
+	if (tree->left != NULL)
+	{
+	h_left = 1 + binary_tree_height(tree->left);
+	}
+
+	if (tree->right != NULL)
+        {
+        h_right = 1 + binary_tree_height(tree->left);
+        }
+
+	if (h_left > h_right)
+	{
+	return h_left;
+	}
+	else
+	{
+	return h_right;
+	}
+}
+
