ee2b904aac6647718096c4909c9114026a11c6f9
prisca2920 <prisca2920@gmail.com>
2024-01-03 01:21:44 -0800

    inserting left child

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,3 +8,22 @@
  */
 
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+{
+	binary_tree_t *newnode;
+
+	if (parent == NULL)
+	{
+	return (NULL);
+	}
+
+	newnode = binary_tree_node(parent, value);
+
+	if (parent->left != NULL)
+	{
+		newnode->left = parent->left;
+		newnode->left->parent = newnode;
+	}
+	parent->left = newnode;
+
+	return (newnode);
+}
