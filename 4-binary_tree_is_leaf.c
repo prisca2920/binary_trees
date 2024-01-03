b52d76f99e0aa985efc96bc1f361a59831594222
prisca2920 <prisca2920@gmail.com>
2024-01-03 01:43:06 -0800

    is a lef

--- 4-binary_tree_is_leaf.c
+++ 4-binary_tree_is_leaf.c
@@ -7,3 +7,16 @@
  */
 
 int binary_tree_is_leaf(const binary_tree_t *node)
+{
+	if (node == NULL)
+	{
+	return (0);
+	}
+
+	if (node->left == NULL && node->right == NULL)
+	{
+	return (1);
+	}
+
+	return (0);
+}
