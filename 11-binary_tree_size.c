9088a874b868b059ef6b36c4af295725e1111c54
prisca2920 <prisca2920@gmail.com>
2024-01-03 22:11:58 -0800

    size

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -7,3 +7,15 @@
  */
 
 size_t binary_tree_size(const binary_tree_t *tree)
+{
+	size_t size;
+
+	if (tree == NULL)
+	{
+	return (0);
+	}
+
+	size = binary_tree_size(tree->left) + 1 + binary_tree_size(tree->right);
+
+	return (size);
+}
