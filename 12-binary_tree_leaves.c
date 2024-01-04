a310ea32d0c3fa6f41e1489f13bd7c1ef490e243
prisca2920 <prisca2920@gmail.com>
2024-01-03 22:23:31 -0800

    leaves

--- 12-binary_tree_leaves.c
+++ 12-binary_tree_leaves.c
@@ -7,3 +7,19 @@
  */
 
 size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	size_t leaves = 0;
+
+	if (tree == NULL)
+	{
+	return (0);
+	}
+
+	if (tree->left == NULL && tree->right == NULL)
+	{
+	return (1);
+	}
+
+	leaves = binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right);
+	return (leaves);
+}
