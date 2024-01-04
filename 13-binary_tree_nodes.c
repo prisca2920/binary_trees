9de2b3f58500ad0e0090e0311b710877508f1eb7
prisca2920 <prisca2920@gmail.com>
2024-01-03 22:41:49 -0800

    nodes

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -7,3 +7,19 @@
  */
 
 size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	size_t nodes;
+
+	if (tree == NULL)
+	{
+	return (0);
+	}
+
+	if (!tree->left && !tree->right)
+	{
+	return (0);
+	}
+
+	nodes = binary_tree_nodes(tree->left) + 1 + binary_tree_nodes(tree->right);
+	return (nodes);
+}
